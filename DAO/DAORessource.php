d8cca50d1d8477977b955e2957402930eba00542
lord-val <codixos@gmail.com>
2018-12-06 23:49:10 +0100

    modif connexion BDD

--- DAO/DAORessource.php
+++ DAO/DAORessource.php
@@ -1,5 +1,33 @@
 <?php
+require_once "ModeleRessource";
 class DAORessource {
+    private static $bdd; //une instance de PDO pour accéder à la base de données
+    public function __construct(){}
+
+    public function setBd(PDO $bd)
+    {
+        $bdd = $bd;
+    }
+
+    public static function ajouter(Ressource $ressource)
+    {
+        $bdd = ConnexionBDD::getConnection();
+        $requete = $bdd->prepare('INSERT INTO Ressource(id, categorie, nom, quantite) VALUES(:id, :categorie, :nom, :quantite)');
+        $requete->bindValue(':id', $ressource->getId());
+        $requete->bindValue(':categorie', $ressource->getCategorie());
+        $requete->bindValue(':nom', $ressource->getNom());
+        $requete->bindValue(':quantite', $ressource->getQuantite());
+        $requete->execute();
+    }
+
+    public static function supprimer(Groupe $groupe)
+    {
+        $bdd = ConnexionBDD::getConnection();
+        $requete = $bdd->prepare('DELETE FROM groupe WHERE nom = :nom and chefDeGroupe = :chefDeGroupe');
+        $requete->bindValue(':nom', $groupe->getNom());
+        $requete->bindValue(':chefDeGroupe', $groupe->getChefDeGroupe());
+        $requete->execute();
+    }
 
 }
 ?>
