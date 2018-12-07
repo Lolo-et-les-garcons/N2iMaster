fa10654a953639e880ff4f0c8c87240384007c17
Flo <florent.dumoulin@etu.univ-tours.fr>
2018-12-07 01:48:31 +0100

    des supers animations avec des trucs randoms

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
    flag = 1,
    height = 65;
 	jauge = $(".jauge>div");
+
+   //barre d'énergie lul
 	input.change(function() {
 		if($(this).is(":checked")) {
 			nbCheck += 1;
@@ -35,4 +37,21 @@ $(document).ready(function() {
       }
       jauge.css("height", height+"%");
    }, 2500);
+
+   //Température
+   var temp = Math.floor(Math.random()*15+35), r, g;
+   r = temp*5;
+   g = (1.1-(temp/50))*255;
+   $(".indicateur1").css("background-color", "rgb("+r+","+g+",16)");
+   $(".tempSoleil>div:first-child>p").text("Température : "+temp+"°C");
+
+   //vent
+   var vent = Math.floor(Math.random()*30+15), duration;
+   duration = (1-((vent-15)/30))*3+3;
+   $(".vent>img").css("animation-duration", duration+"s");
+   $(".vent>h2").text(vent+"km/h");
+
+   //ensoleillement
+   var sol = 128 + Math.floor(Math.random()*128);
+   $(".indicateur2").css("background-color", "rgb(192,"+sol+",0)");
 });
